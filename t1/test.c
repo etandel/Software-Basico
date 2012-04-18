7b9e1a214a9537624a5f688d3ccf19d67be08037
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-04-18 16:45:48 -0300

    Create utf32_8 output for both kinds of inputs.
      The test now opens both utf32 files (big and little endian) and
      generates one output file for each of them.

--- t1/test.c
+++ t1/test.c
@@ -7,8 +7,9 @@ void dump (void *p, int n);
 int main(void){
     FILE * u32_big = fopen("examples/utf32_big.txt", "rb");
     FILE * u32_lit = fopen("examples/utf32_lit.txt", "rb");
-    FILE * u8      = fopen("examples/utf8_demo.txt", "rb");
-    FILE * dump    = fopen("dump.txt", "rb");
+
+    FILE * u8_from_big = fopen("big_to_u8.txt", "wb");
+    FILE * u8_from_lit = fopen("lit_to_u8.txt", "wb");
     //char seq[4];
 
     //char big_32_e  = endian(u32_big);
@@ -17,10 +18,10 @@ int main(void){
     // printf("utf32_lit end: %c\n", lit_32_e < 0 ? 'E' : lit_32_e);
 
 
-    utf32_8(u32_lit, dump);
+    utf32_8(u32_big, u8_from_big);
+    utf32_8(u32_lit, u8_from_lit);
 
-    fclose(u32_big);
-    fclose(u32_lit);
-    fclose(u8);
+    fclose(u8_from_big); fclose(u8_from_lit);
+    fclose(u32_big);     fclose(u32_lit);
     return 0;
 }
