aa3aebd1a86c6e7b5f6ee14b7079ea39d5285eb1
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-04-22 01:26:14 -0300

    Fix test.

--- t1/test.c
+++ t1/test.c
@@ -2,10 +2,12 @@
 #include "utft.h"
 #include <stdio.h>
 
-void dump (void *p, int n);
+void dump_char (void *p, int n, char *sep);
 
 int main(void){
     FILE * u8 = fopen("examples/utf8_demo.txt", "rb");
+    FILE * lit_from_u8 = fopen("u8_to_lit.txt", "wb");
+    FILE * big_from_u8 = fopen("u8_to_big.txt", "wb");
 
     //FILE * u32_big = fopen("examples/utf32_err.txt", "rb");
     //FILE * u32_lit = fopen("examples/utf32_lit.txt", "rb");
@@ -14,7 +16,11 @@ int main(void){
     //FILE * u8_from_lit = fopen("lit_to_u8.txt", "wb");
     //char seq[4];
 
-    utf8_32(u8, stdout, 1);
+    utf8_32(u8, big_from_u8, 1);
+
+
+    rewind(u8);
+    utf8_32(u8, lit_from_u8, 0);
 
     //utf32_8(u32_big, u8_from_big);
     //utf32_8(u32_lit, stdout);
@@ -22,6 +28,8 @@ int main(void){
     //fclose(u32_big); fclose(u8_from_big);
     //fclose(u8_from_lit); fclose(u32_lit);
     fclose(u8);
+    fclose(lit_from_u8);
+    fclose(big_from_u8);
 
     return 0;
 }
