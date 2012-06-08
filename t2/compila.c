a69d26d6f5ae94700928c2e81bbe701c470e0729
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-06-07 23:29:46 -0300

    Add compila func and test file.

--- t2/compila.c
+++ t2/compila.c
@@ -1,2 +1,8 @@
 /*Elias Tandel Barrionovo  1010270 3WA*/
 /*João Pedro Fabris Campos 1010273 3WA*/
+
+#include "compila.h"
+
+funcp compila(FILE *src){
+    return NULL;
+}
---
+++ t2/teste.c
@@ -0,0 +1,10 @@
+#include "compila.h"
+#include <stdio.h>
+
+int main(const int argc, const char *argv[]){
+    FILE * source = fopen(argv[1], "r");
+    funcp compiled = compila(source);
+    int ret = compiled()
+    printf("%s => %d\n", argv[1], ret);
+    return 0;
+}
