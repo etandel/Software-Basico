77a6898c0415b5c50ea3c957bdf70e2dab28de56
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-06-07 23:40:16 -0300

    Add makefile and a few testcases.

--- t2/test.c
+++ t2/test.c
@@ -4,7 +4,7 @@
 int main(const int argc, const char *argv[]){
     FILE * source = fopen(argv[1], "r");
     funcp compiled = compila(source);
-    int ret = compiled()
+    int ret = compiled();
     printf("%s => %d\n", argv[1], ret);
     return 0;
 }
