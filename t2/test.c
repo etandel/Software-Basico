f2e5e3b9769ce97f3dbdc79be92c46e395b1c99e
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-06-13 20:19:35 -0300

    Free generated function after execution.

--- t2/test.c
+++ t2/test.c
@@ -1,11 +1,17 @@
 #include "compila.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(const int argc, const char *argv[]){
     FILE * source = fopen(argv[1], "r");
-    funcp compiled = compila(source);
-    int ret = compiled();
+    funcp compiled;
+    int ret;
+
+    compiled = compila(source);
+    ret = compiled();
     printf("%d\n", ret);
+    free(compiled);
+
     return 0;
 }
 
