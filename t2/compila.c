447c5fc59bba024f9340c430f41c84ca0ff93af3
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-06-18 01:40:08 -0300

    Remove Int union. Add dump function. Refactor copying of code.

--- t2/compila.c
+++ t2/compila.c
@@ -2,40 +2,47 @@
 /*João Pedro Fabris Campos 1010273 3WA*/
 
 #include "compila.h"
-#include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
-static union integer {
-    unsigned char c[4];
-    int i;
-};
-typedef union integer Int;
+static void dump(unsigned char *c, size_t n){
+    unsigned char *p;
+    for (p=c; n>0; n--, p++)
+        printf("%x ", *p);
+    putchar('\n');
+    
+}
+
+void set_head(unsigned char *code, int *offset){
+    // inital push and move
+    unsigned char begin[3] = {0x55U,0x89U, 0xe5U};
+    memcpy(code+*offset, begin, 3);
+    *offset += 3;
+}
+
+void set_tail(unsigned char *code, int *offset){
+    // final move, push and ret
+    unsigned char end[4] = {0x89U, 0xecU, 0x5dU, 0xc3U};
+    memcpy(code+*offset, end, 4);
+    *offset += 4;
+}
 
 funcp compila(FILE *src){
     int offset=0, i;
     size_t code_size = 12;
 
-    Int ret_val;
-    unsigned char
-        begin[3] = {0x55U,0x89U, 0xe5U},
-        end[4] = {0x89U, 0xecU, 0x5dU, 0xc3U};
-
+    int ret_val;
+    
     unsigned char *code = (unsigned char*)malloc(code_size*sizeof(unsigned char));
 
-    // inital push and move
-    for (i=0; i<3; i++, offset++)
-        code[offset] = begin[i];
+    set_head(code, &offset);
 
     fscanf(src, "ret $%d", &ret_val);
     code[offset++] = 0xb8U;
+    memcpy(code+offset, &ret_val, sizeof(int));
+    offset += 4;
 
-    for (i=0; i<4; i++, offset++) \
-        code[offset] = ret_val.c[i];
-
-
-    // final move, push and ret
-    for (i=0; i<4; i++, offset++)
-        code[offset] = end[i];
+    set_tail(code, &offset);
 
     return (funcp)code;
 }
